a9fe50ca098724f21539bc945181c8aca74709c9
qq_35969791 <qq_35969791@noreply.gitcode.com>
2025-04-15 00:13:14 +0800

    icon

--- src/backend/installer/linux/postinst.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# 创建桌面快捷方式
-cp /opt/BixChat/bixchat.desktop /usr/share/applications/
-update-desktop-database
--- src/backend/installer/linux/rerm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# 移除桌面快捷方式
-rm -f /usr/share/applications/bixchat.desktop
-update-desktop-database
